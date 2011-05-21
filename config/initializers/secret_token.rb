48d8e822bd3da46a60b89470a40ead2e4cefdc42
Stanislav Seifert <seifert.stanislav@googlemail.com>
2011-05-21 14:15:15 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Graph::Application.config.secret_token = '4522fc603a0e5079a7a947a3c467065aab03dafe5a57d16072d0398c26bf33a9e9f02a564e3ad38c2e6db23321dc2db714213c3efc1f9afb0b98a7453f255fa1'
